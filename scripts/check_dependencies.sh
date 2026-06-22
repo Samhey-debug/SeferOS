928251c7fdcf538b287f2b3828abd51ebf03170c
#별　star ` <HeyItsSamhey@outlook.com>
2026-06-22 17:02:16 +0100

    Delete scripts directory

--- scripts/check_dependencies.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-set -euo pipefail
-
-if [[ ! -d "edk2-ovmf-bins" && ! -d "limine-binary" ]]; then
-	exit 1
-fi
--- scripts/install.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-set -euo pipefail
-
-curl -L https://github.com/osdev0/edk2-ovmf-stable-bins/releases/latest/download/edk2-ovmf-bins.tar.gz | gunzip | tar -xf -
-curl -L https://github.com/Limine-Bootloader/Limine/releases/latest/download/limine-binary.tar.gz | gunzip | tar -xf -
-
-gcc limine-binary/limine.c -o limine
